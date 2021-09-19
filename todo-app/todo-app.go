be984183d09da67bd2a90b3b185eac0ad7d89511
Marco <marco.wettstein@gmail.com>
2021-09-19 18:36:23 +0200

    serve a html page in todo app

--- todo-app/todo-app.go
+++ todo-app/todo-app.go
@@ -11,10 +11,16 @@ import (
 func main() {
 	fmt.Printf("Server started, listening on %s\n", os.Getenv("PORT"))
 	r := gin.Default()
+	r.LoadHTMLGlob("index.html")
+
 	r.GET("ping", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
 			"message": "pong",
 		})
+	})
+
+	r.GET("/", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "index.html", nil)
 	})
 	r.Run()
 }
